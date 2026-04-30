e803dff4781bc8c62e8bc8ae1b8143bf39390bbc
DinaCahyoWibowo <dinacahyo35@gmail.com>
2026-04-30 20:18:37 +0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('demo:reset')->everyMinute(0);
+        $schedule->command('demo:reset')->hourlyAt(0);
     }
 
     /**
